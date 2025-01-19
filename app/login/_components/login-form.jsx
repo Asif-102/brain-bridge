f14aeec9fd0218efd306a7f7b5a28afcee1a0fc4
Asif-102 <asif1zero2@gmail.com>
2025-01-19 14:53:08 +0600

    user login feature complete

--- app/login/_components/login-form.jsx
+++ app/login/_components/login-form.jsx
@@ -1,5 +1,10 @@
+"use client";
+
 import Link from "next/link";
 
+import { credentialLogin } from "@/app/actions";
+import { AlertDestructive } from "@/components/alert-destructive";
+import { ButtonLoading } from "@/components/button-loading";
 import { Button } from "@/components/ui/button";
 import {
   Card,
@@ -10,8 +15,39 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export function LoginForm() {
+  const router = useRouter();
+
+  const [loader, setLoader] = useState(false);
+  const [error, setError] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setLoader(true);
+    setError("");
+
+    try {
+      const formData = new FormData(event.currentTarget);
+      const response = await credentialLogin(formData);
+      console.log("🚀 ~ onSubmit ~ response:", response);
+
+      if (!!response.error) {
+        console.error(response.error);
+        setError(response.error);
+      } else {
+        router.push("/courses");
+      }
+    } catch (e) {
+      console.log(e.message);
+      setError(e.message);
+    } finally {
+      setLoader(false);
+    }
+  };
+
   return (
     <Card className="mx-auto max-w-sm w-full">
       <CardHeader>
@@ -19,31 +55,36 @@ export function LoginForm() {
         <CardDescription>
           Enter your email below to login to your account
         </CardDescription>
+        {error && <AlertDestructive>{error}</AlertDestructive>}
       </CardHeader>
       <CardContent>
-        <div className="grid gap-4">
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input
-              id="email"
-              type="email"
-              placeholder="m@example.com"
-              required
-            />
-          </div>
-          <div className="grid gap-2">
-            <div className="flex items-center">
-              <Label htmlFor="password">Password</Label>
-              {/* <Link href="#" className="ml-auto inline-block text-sm underline">
-                Forgot your password?
-              </Link> */}
+        <form onSubmit={onSubmit}>
+          <div className="grid gap-4">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Email</Label>
+              <Input
+                id="email"
+                name="email"
+                type="email"
+                placeholder="m@example.com"
+                required
+              />
             </div>
-            <Input id="password" type="password" required />
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input id="password" name="password" type="password" required />
+            </div>
+            {loader ? (
+              <ButtonLoading />
+            ) : (
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+            )}
           </div>
-          <Button type="submit" className="w-full">
-            Login
-          </Button>
-        </div>
+        </form>
         <div className="mt-4 text-center text-sm">
           Don&apos;t have an account?{" "}
           <Link href="register" className="underline">
--- auth.js
+++ auth.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import { authConfig } from "./auth.config";
-import { userModel } from "./model/user-model";
+import { User } from "./model/user-model";
 import dbConnect from "./service/mongo";
 
 export const {
@@ -19,7 +19,7 @@ export const {
 
         try {
           await dbConnect();
-          const user = await userModel.findOne({ email: credentials.email });
+          const user = await User.findOne({ email: credentials.email });
 
           console.log(user);
 
@@ -39,7 +39,7 @@ export const {
             throw new Error("User not found");
           }
         } catch (error) {
-          console.error(err);
+          console.error(error);
           throw new Error(error);
         }
       },
