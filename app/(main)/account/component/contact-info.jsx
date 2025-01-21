49012752572ef66cb3f68d56914cf8b421d195d3
Asif-102 <asif1zero2@gmail.com>
2025-01-22 01:49:57 +0600

    updat contact info

--- app/(main)/account/component/contact-info.jsx
+++ app/(main)/account/component/contact-info.jsx
@@ -1,31 +1,86 @@
+"use client";
+
+import { updateUserInfo } from "@/app/actions/account";
+import { ButtonLoading } from "@/components/button-loading";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { useState } from "react";
+import { toast } from "sonner";
 
-export default function ContactInfo() {
+export default function ContactInfo({ userInfo }) {
+  const [infoState, setInfoState] = useState({
+    phone: userInfo.phone ?? "",
+    socialMedia: userInfo.socialMedia ?? "",
+  });
+
+  const [loader, setLoader] = useState(false);
+
+  const handleChange = (event) => {
+    const field = event.target.name;
+    const value = event.target.value;
+
+    setInfoState({
+      ...infoState,
+      [field]: value,
+    });
+  };
+
+  const handleUpdate = async (event) => {
+    event.preventDefault();
+    setLoader(true);
+
+    try {
+      await updateUserInfo(userInfo?.email, infoState);
+      toast.success("User contact info updated successfully.");
+    } catch (error) {
+      console.error(error);
+      toast.error(`Error: ${error.message}`);
+    } finally {
+      setLoader(false);
+    }
+  };
+
   return (
     <div>
       <h5 className="text-lg font-semibold mb-4">Contact Info :</h5>
-      <form>
+      <form onSubmit={handleUpdate}>
         <div className="grid grid-cols-1 gap-5">
           <div>
             <Label className="mb-2 block">Phone No. :</Label>
             <Input
-              name="number"
+              name="phone"
               id="number"
               type="number"
+              value={infoState.phone}
+              onChange={handleChange}
               placeholder="Phone :"
+              required
             />
           </div>
           <div>
             <Label className="mb-2 block">Website :</Label>
-            <Input name="url" id="url" type="url" placeholder="Url :" />
+            <Input
+              name="socialMedia"
+              value={infoState.socialMedia}
+              onChange={handleChange}
+              id="url"
+              type="url"
+              placeholder="Url :"
+              required
+            />
           </div>
         </div>
         {/*end grid*/}
-        <Button className="mt-5" type="submit">
-          Add
-        </Button>
+        {loader ? (
+          <div className="mt-5">
+            <ButtonLoading />
+          </div>
+        ) : (
+          <Button className="mt-5" type="submit">
+            Update
+          </Button>
+        )}
       </form>
     </div>
   );
