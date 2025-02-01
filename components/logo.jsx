34f129d2722ac4e8469e9b20e6c6e3d9f5d343fa
Asif-102 <asif1zero2@gmail.com>
2025-02-01 22:29:06 +0600

    switch logo based on theme

--- components/logo.jsx
+++ components/logo.jsx
@@ -1,8 +1,19 @@
-import logo from "@/assets/dr_logo_black.svg";
+"use client";
+
+import logo_black from "@/assets/dr_logo_black.svg";
+import logo_white from "@/assets/dr_logo_white.svg";
 import { cn } from "@/lib/utils";
+import { useTheme } from "next-themes";
 import Image from "next/image";
+
 export const Logo = ({ className = "" }) => {
+  const { theme } = useTheme();
+
   return (
-    <Image className={cn("max-w-[100px]", className)} src={logo} alt="logo" />
+    <Image
+      className={cn("max-w-[100px]", className)}
+      src={theme === "dark" ? logo_white : logo_black}
+      alt="logo"
+    />
   );
 };
