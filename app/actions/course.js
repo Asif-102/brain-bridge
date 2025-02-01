52b9b2bc94c2c7c74beaaef4fa28c2c5cccf79a8
Asif-102 <asif1zero2@gmail.com>
2025-02-01 21:34:19 +0600

    Course publish, unpublish and delete

--- app/actions/course.js
+++ app/actions/course.js
@@ -4,6 +4,7 @@ import { getLoggedInUser } from "@/lib/loggedin-user";
 import { Course } from "@/model/course-model";
 import { create } from "@/queries/courses";
 import dbConnect from "@/service/mongo";
+import { del } from "@vercel/blob";
 
 export async function createCourse(data) {
   try {
@@ -23,5 +24,45 @@ export async function updateCourse(courseId, dataToUpdate) {
     await Course.findByIdAndUpdate(courseId, dataToUpdate);
   } catch (e) {
     throw new Error(e);
+  }
+}
+
+export async function changeCoursePublishState(courseId) {
+  try {
+    await dbConnect();
+
+    const course = await Course.findById(courseId);
+    const res = await Course.findByIdAndUpdate(
+      courseId,
+      { active: !course.active },
+      { lean: true }
+    );
+    return res.active;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
+
+export async function deleteCourse(courseId) {
+  try {
+    await dbConnect();
+
+    const course = await Course.findById(courseId);
+    if (!course) {
+      throw new Error("Course not found");
+    }
+
+    if (course?.thumbnail) {
+      try {
+        await del(course.thumbnail);
+        console.log("Deleted old thumbnail:", course.thumbnail);
+      } catch (error) {
+        console.error("Error deleting old thumbnail:", error);
+      }
+    }
+
+    await Course.findByIdAndDelete(courseId);
+  } catch (err) {
+    throw new Error(err);
   }
 }
